24749fbf21cf262dfa4d24df4715bafda1112d4f
Jonas Sjömark <jonas.sjomark@kungsbacka.se>
2019-03-25 11:59:59 +0100

    Code layout changes

--- Kungsbacka.AccountTasks/TaskFactory.cs
+++ Kungsbacka.AccountTasks/TaskFactory.cs
@@ -34,202 +34,202 @@ namespace Kungsbacka.AccountTasks
             switch (taskName)
             {
                 case "Wait" :
-                {
-                    long? minutes = dictionary.GetValueOrDefault<long?>("Minutes");
-                    returnTask = new WaitTask(minutes);
-                    break;
-                }
+                    {
+                        long? minutes = dictionary.GetValueOrDefault<long?>("Minutes");
+                        returnTask = new WaitTask(minutes);
+                        break;
+                    }
                 case "MsolEnableSync":
-                {
-                    returnTask = new MsolEnableSyncTask();
-                    break;
-                }
+                    {
+                        returnTask = new MsolEnableSyncTask();
+                        break;
+                    }
                 case "MsolLicense":
-                {
-                    returnTask = new MsolLicenseTask(dictionary.GetMsolLicenseArray());
-                    break;
-                }
+                    {
+                        returnTask = new MsolLicenseTask(dictionary.GetMsolLicenseArray());
+                        break;
+                    }
                 case "MsolLicenseGroup":
-                {
-                    bool skipSyncCheck = dictionary.GetValueOrDefault<bool>("SkipSyncCheck");
-                    bool skipDynamicGroupCheck = dictionary.GetValueOrDefault<bool>("SkipDynamicGroupCheck");
-                    returnTask = new MsolLicenseGroupTask(dictionary.GetGuidArray("LicenseGroups"), skipSyncCheck, skipDynamicGroupCheck);
-                    break;
-                }
+                    {
+                        bool skipSyncCheck = dictionary.GetValueOrDefault<bool>("SkipSyncCheck");
+                        bool skipDynamicGroupCheck = dictionary.GetValueOrDefault<bool>("SkipDynamicGroupCheck");
+                        returnTask = new MsolLicenseGroupTask(dictionary.GetGuidArray("LicenseGroups"), skipSyncCheck, skipDynamicGroupCheck);
+                        break;
+                    }
                 case "MsolRemoveLicenseGroup":
-                {
-                    bool skipBaseLicenseCheck = dictionary.GetValueOrDefault<bool>("SkipBaseLicenseCheck");
-                    returnTask = new MsolRemoveLicenseGroupTask(dictionary.GetGuidArray("LicenseGroups"), skipBaseLicenseCheck);
-                    break;
-                }
+                    {
+                        bool skipBaseLicenseCheck = dictionary.GetValueOrDefault<bool>("SkipBaseLicenseCheck");
+                        returnTask = new MsolRemoveLicenseGroupTask(dictionary.GetGuidArray("LicenseGroups"), skipBaseLicenseCheck);
+                        break;
+                    }
                 case "MsolRemoveAllLicenseGroup":
-                {
-                    bool skipStashLicense = dictionary.GetValueOrDefault<bool>("SkipStashLicense");
-                    returnTask = new MsolRemoveAllLicenseGroupTask(skipStashLicense);
-                    break;
-                }
+                    {
+                        bool skipStashLicense = dictionary.GetValueOrDefault<bool>("SkipStashLicense");
+                        returnTask = new MsolRemoveAllLicenseGroupTask(skipStashLicense);
+                        break;
+                    }
                 case "MsolRestoreLicenseGroup":
-                {
-                    bool skipSyncCheck = dictionary.GetValueOrDefault<bool>("SkipSyncCheck");
-                    returnTask = new MsolRestoreLicenseGroupTask(skipSyncCheck);
-                    break;
-                }
+                    {
+                        bool skipSyncCheck = dictionary.GetValueOrDefault<bool>("SkipSyncCheck");
+                        returnTask = new MsolRestoreLicenseGroupTask(skipSyncCheck);
+                        break;
+                    }
                 case "MicrosoftOnlineRestore":
-                {
-                    string mailboxTypeString = dictionary.GetValueOrDefault<string>("Type");
-                    if (!Enum.TryParse(mailboxTypeString, out MailboxType mailboxType))
                     {
-                        throw new ArgumentException("Type");
+                        string mailboxTypeString = dictionary.GetValueOrDefault<string>("Type");
+                        if (!Enum.TryParse(mailboxTypeString, out MailboxType mailboxType))
+                        {
+                            throw new ArgumentException("Type");
+                        }
+                        returnTask = new MicrosoftOnlineRestoreTask(mailboxType);
+                        break;
                     }
-                    returnTask = new MicrosoftOnlineRestoreTask(mailboxType);
-                    break;
-                }
                 case "EnableMailbox":
-                {
-                    returnTask = new EnableMailboxTask(dictionary.GetNullableMailboxType());
-                    break;
-                }
+                    {
+                        returnTask = new EnableMailboxTask(dictionary.GetNullableMailboxType());
+                        break;
+                    }
                 case "DisableMailbox":
-                {
-                    returnTask = new DisableMailboxTask();
-                    break;
-                }
+                    {
+                        returnTask = new DisableMailboxTask();
+                        break;
+                    }
                 case "ConnectMailbox":
-                {
-                    returnTask = new ConnectMailboxTask();
-                    break;
-                }
+                    {
+                        returnTask = new ConnectMailboxTask();
+                        break;
+                    }
                 case "ConfigureMailbox":
-                {
-                    returnTask = new ConfigureMailboxTask(dictionary.GetNullableMailboxType());
-                    break;
-                }
+                    {
+                        returnTask = new ConfigureMailboxTask(dictionary.GetNullableMailboxType());
+                        break;
+                    }
                 case "ConfigureOwa":
-                {
-                    returnTask = new ConfigureOwaTask(dictionary.GetNullableMailboxType());
-                    break;
-                }
+                    {
+                        returnTask = new ConfigureOwaTask(dictionary.GetNullableMailboxType());
+                        break;
+                    }
                 case "ConfigureOnlineMailbox":
-                {
-                    returnTask = new ConfigureOnlineMailboxTask(dictionary.GetNullableMailboxType());
-                    break;
-                }
+                    {
+                        returnTask = new ConfigureOnlineMailboxTask(dictionary.GetNullableMailboxType());
+                        break;
+                    }
                 case "ConfigureOnlineOwa":
-                {
-                    returnTask = new ConfigureOnlineOwaTask();
-                    break;
-                }
+                    {
+                        returnTask = new ConfigureOnlineOwaTask();
+                        break;
+                    }
                 case "ConfigureCalendar":
-                {
-                    returnTask = new ConfigureCalendarTask(dictionary.GetNullableMailboxType());
-                    break;
-                }
+                    {
+                        returnTask = new ConfigureCalendarTask(dictionary.GetNullableMailboxType());
+                        break;
+                    }
                 case "ConfigureMessage":
-                {
-                    returnTask = new ConfigureMessageTask();
-                    break;
-                }
+                    {
+                        returnTask = new ConfigureMessageTask();
+                        break;
+                    }
                 case "CleanupMailbox":
-                {
-                    returnTask = new CleanupMailboxTask();
-                    break;
-                }
+                    {
+                        returnTask = new CleanupMailboxTask();
+                        break;
+                    }
                 case "ConfigureMailboxAutoReplyTask":
-                {
-                    if (!dictionary.ContainsKey("Enabled"))
                     {
-                        throw new ArgumentException("Enabled");
+                        if (!dictionary.ContainsKey("Enabled"))
+                        {
+                            throw new ArgumentException("Enabled");
+                        }
+                        bool enabled = (bool)dictionary["Enabled"];
+                        string message = dictionary.GetValueOrDefault<string>("Message");
+                        returnTask = new ConfigureMailboxAutoReplyTask(enabled, message);
+                        break;
                     }
-                    bool enabled = (bool)dictionary["Enabled"];
-                    string message = dictionary.GetValueOrDefault<string>("Message");
-                    returnTask = new ConfigureMailboxAutoReplyTask(enabled, message);
-                    break;
-                }
                 case "SendWelcomeMail":
-                {
-                    returnTask = new SendWelcomeMailTask(dictionary.GetNullableMailboxType());
-                    break;
-                }
+                    {
+                        returnTask = new SendWelcomeMailTask(dictionary.GetNullableMailboxType());
+                        break;
+                    }
                 case "EnableRemoteMailbox":
-                {
-                    returnTask = new EnableRemoteMailboxTask();
-                    break;
-                }
+                    {
+                        returnTask = new EnableRemoteMailboxTask();
+                        break;
+                    }
                 case "ConfigureRemoteMailbox":
-                {
-                    returnTask = new ConfigureRemoteMailboxTask();
-                    break;
-                }
+                    {
+                        returnTask = new ConfigureRemoteMailboxTask();
+                        break;
+                    }
                 case "SamlId":
-                {
-                    returnTask = new SamlIdTask();
-                    break;
-                }
+                    {
+                        returnTask = new SamlIdTask();
+                        break;
+                    }
                 case "OnpremMailbox":
-                {
-                    string mailboxTypeString = dictionary.GetValueOrDefault<string>("Type");
-                    if (!Enum.TryParse(mailboxTypeString, out MailboxType mailboxType))
                     {
-                        throw new ArgumentException("Type");
+                        string mailboxTypeString = dictionary.GetValueOrDefault<string>("Type");
+                        if (!Enum.TryParse(mailboxTypeString, out MailboxType mailboxType))
+                        {
+                            throw new ArgumentException("Type");
+                        }
+                        returnTask = new OnpremMailboxTask(
+                            mailboxType,
+                            GetTaskSequence(dictionary)
+                        );
+                        break;
                     }
-                    returnTask = new OnpremMailboxTask(
-                        mailboxType,
-                        GetTaskSequence(dictionary)
-                    );
-                    break;
-                }
                 case "ReconfigureOnpremMailbox":
-                {
-                    string mailboxTypeString = dictionary.GetValueOrDefault<string>("Type");
-                    if (!Enum.TryParse(mailboxTypeString, out MailboxType mailboxType))
                     {
-                        throw new ArgumentException("Type");
+                        string mailboxTypeString = dictionary.GetValueOrDefault<string>("Type");
+                        if (!Enum.TryParse(mailboxTypeString, out MailboxType mailboxType))
+                        {
+                            throw new ArgumentException("Type");
+                        }
+                        returnTask = new ReconfigureOnpremMailboxTask(
+                            mailboxType,
+                            GetTaskSequence(dictionary)
+                        );
+                        break;
                     }
-                    returnTask = new ReconfigureOnpremMailboxTask(
-                        mailboxType,
-                        GetTaskSequence(dictionary)
-                    );
-                    break;
-                }
                 case "ReconnectOnpremMailbox":
-                {
-                    string mailboxTypeString = dictionary.GetValueOrDefault<string>("Type");
-                    if (!Enum.TryParse(mailboxTypeString, out MailboxType mailboxType))
                     {
-                        throw new ArgumentException("Type");
+                        string mailboxTypeString = dictionary.GetValueOrDefault<string>("Type");
+                        if (!Enum.TryParse(mailboxTypeString, out MailboxType mailboxType))
+                        {
+                            throw new ArgumentException("Type");
+                        }
+                        returnTask = new ReconnectOnpremMailboxTask(
+                            mailboxType,
+                            GetTaskSequence(dictionary)
+                        );
+                        break;
                     }
-                    returnTask = new ReconnectOnpremMailboxTask(
-                        mailboxType,
-                        GetTaskSequence(dictionary)
-                    );
-                    break;
-                }
                 case "MicrosoftOnline":
-                {
-                    returnTask = new MicrosoftOnlineTask(
-                        dictionary.GetGuidArray("LicenseGroups"),
-                        GetTaskSequence(dictionary)
-                    );
-                    break;
-                }
-                case "MicrosoftOnlineWithMailbox":
-                {
-                    string mailboxTypeString = dictionary.GetValueOrDefault<string>("Type");
-                    if (!Enum.TryParse(mailboxTypeString, out MailboxType mailboxType))
                     {
-                        throw new ArgumentException("Type");
+                        returnTask = new MicrosoftOnlineTask(
+                            dictionary.GetGuidArray("LicenseGroups"),
+                            GetTaskSequence(dictionary)
+                        );
+                        break;
                     }
-                    returnTask = new MicrosoftOnlineWithMailboxTask(
-                        mailboxType,
-                        dictionary.GetGuidArray("LicenseGroups"),
-                        GetTaskSequence(dictionary)
-                    );
-                    break;
-                }
+                case "MicrosoftOnlineWithMailbox":
+                    {
+                        string mailboxTypeString = dictionary.GetValueOrDefault<string>("Type");
+                        if (!Enum.TryParse(mailboxTypeString, out MailboxType mailboxType))
+                        {
+                            throw new ArgumentException("Type");
+                        }
+                        returnTask = new MicrosoftOnlineWithMailboxTask(
+                            mailboxType,
+                            dictionary.GetGuidArray("LicenseGroups"),
+                            GetTaskSequence(dictionary)
+                        );
+                        break;
+                    }
                 default:
-                {
-                    throw new ArgumentException(string.Format("Invalid TaskName: {0}", taskName));
-                }
+                    {
+                        throw new ArgumentException(string.Format("Invalid TaskName: {0}", taskName));
+                    }
             }
             returnTask.Id = dictionary.GetValueOrDefault<long?>("Id");
             if (returnTask is BasicTask)
